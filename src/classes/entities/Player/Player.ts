e34ec5f450a70a6ed21723ac804385e461b659f2
Derek Surguy <Surguy.Derek@gmail.com>
2020-10-27 10:47:42 -0500

    Just use the actual angle for moving to a point instead of facing

--- src/classes/entities/Player/Player.ts
+++ src/classes/entities/Player/Player.ts
@@ -1,9 +1,15 @@
 import Entity from "../../Entity";
 import {playerSpriteFactory} from "./factories";
-import {PlayerFacing} from "./types";
+
+const normalizeAngle = (angle: number) => {
+  let normalizedAngle = angle;
+  while( normalizedAngle < 0 ) normalizedAngle += 360;
+  while ( normalizedAngle > 360 ) normalizedAngle -= 360;
+  return normalizedAngle;
+}
 
 export default class Player extends Entity {
-  private _facing: PlayerFacing = PlayerFacing.up;
+  private _facing: number = 0;
 
   constructor() {
     super();
@@ -13,17 +19,8 @@ export default class Player extends Entity {
   get facing() {
     return this._facing;
   }
-  set facing(value) {
-    this._facing = value;
-    switch(this._facing) {
-      case PlayerFacing.up: this.sprite.angle = 0; break;
-      case PlayerFacing.upRight: this.sprite.angle = 45; break;
-      case PlayerFacing.right: this.sprite.angle = 90; break;
-      case PlayerFacing.downRight: this.sprite.angle = 135; break;
-      case PlayerFacing.down: this.sprite.angle = 180; break;
-      case PlayerFacing.downLeft: this.sprite.angle = 225; break;
-      case PlayerFacing.left: this.sprite.angle = 270; break;
-      case PlayerFacing.upLeft: this.sprite.angle = 315; break;
-    }
+  set facing(angle) {
+    this._facing = normalizeAngle(angle);
+    this.sprite.angle = this._facing;
   }
 }
--- src/logic/interaction/gameLoopHooks/movePlayerToQueuedLocation.ts
+++ src/logic/interaction/gameLoopHooks/movePlayerToQueuedLocation.ts
@@ -1,28 +1,11 @@
-import {PlayerFacing} from "../../../classes/entities/Player/types";
 import angleBetweenPoints from "../../../utils/angleBetweenPoints";
 import {Point} from "../../../types";
 import GameState from "../../GameState";
 
-const angleToFacing = [
-  [0, 22.5, PlayerFacing.left],
-  [45-22.5, 45+22.5, PlayerFacing.upLeft],
-  [90-22.5, 90+22.5, PlayerFacing.up],
-  [135-22.5, 135+22.5, PlayerFacing.upRight],
-  [180-22.5, 180+22.5, PlayerFacing.right],
-  [225-22.5, 225+22.5, PlayerFacing.downRight],
-  [270-22.5, 270+22.5, PlayerFacing.down],
-  [315-22.5, 315+22.5, PlayerFacing.downLeft],
-  [360-22.5, 360, PlayerFacing.left]
-];
+const BASE_PLAYER_SPEED = 20;
 
 export default function movePlayerToQueueLocation(delta: number, gameState: GameState) {
   if( gameState.playerMovement.moveTo ){
-    const angle = angleBetweenPoints(gameState.player.sprite.position as Point, gameState.playerMovement.moveTo);
-    for( let [lowerBound, upperBound, facing] of angleToFacing ){
-      if( angle >= lowerBound && angle < upperBound ){
-        gameState.player.facing = facing;
-        break;
-      }
-    }
+    gameState.player.facing = angleBetweenPoints(gameState.player.sprite.position as Point, gameState.playerMovement.moveTo) - 90;
   }
 }
